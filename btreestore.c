043551ca67e76624c97ebd6ffad839991fbda2a6
welu3614 <welu3614@uni.sydney.edu.au>
2021-05-30 15:56:50 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -77,7 +77,7 @@ void decrypt_tea(uint32_t cipher[2], uint32_t plain[2], uint32_t key[4]) {
 }
 
 void encrypt_tea_ctr(uint64_t * plain, uint32_t key[4], uint64_t nonce, uint64_t * cipher, uint32_t num_blocks) {
-    for (int i = 0; i < num_blocks; i++) {
+    for (int i = 0; i < num_blocks-1; i++) {
         uint64_t tmp1 = i ^ nonce;
         uint64_t tmp2;
         encrypt_tea(tmp1, tmp2, key);
