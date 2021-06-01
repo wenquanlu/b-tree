e97cd999369f7044a8bb7fca6d858c7821d40eae
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 09:41:58 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -198,6 +198,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
 
     if (root -> parent == NULL && root -> num_keys > branching + 1) {
         int midindex = (root -> num_keys - 1)/2;
+        printf("midindedx: %d\n", midindex);
         int midindex_key = (root -> pairs)[midindex].key;
         int num_key_left = midindex;
         int num_key_right = root -> num_keys - midindex - 1;
@@ -205,7 +206,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         struct tree_node * original_child_ptr = root -> children;
         struct kv_pair * original_kv_ptr = root -> pairs;
 
-        root -> children = realloc(root -> children, 2 * sizeof(struct tree_node));
+        root -> children = malloc(2 * sizeof(struct tree_node));
         struct tree_node * left_node = root -> children;
         struct tree_node * right_node = (root -> children) + 1;
 
