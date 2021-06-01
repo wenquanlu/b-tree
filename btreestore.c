a708b0bccad4fe52dc6c9bc7843b41d753d47032
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 09:45:56 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -211,6 +211,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         right_node -> num_keys = num_key_right;
         fprintf(stderr, "num key right: %d\n", num_key_right);
         right_node -> children = malloc(sizeof(struct tree_node) * (num_key_right + 1));
+        fprintf(stderr, "malloced pointer %p\n", right_node -> children);
         memcpy(right_node -> children, original_child_ptr + midindex + 1, sizeof(struct tree_node) * (num_key_right + 1));
         memcpy(right_node -> pairs, original_kv_ptr + midindex + 1, num_key_right);
         right_node -> parent = root;
