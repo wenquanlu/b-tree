6ff1b12330153b43d7183fee132047626cdb5c1f
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 09:56:46 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -216,6 +216,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         if (original_child_ptr != NULL) {
             memcpy(right_node -> children, original_child_ptr + midindex + 1, sizeof(struct tree_node) * (num_key_right + 1));
         }
+        fprintf(stderr, "original kv pointer: %p\n", original_kv_ptr);
         memcpy(right_node -> pairs, original_kv_ptr + midindex + 1, num_key_right * sizeof(struct kv_pair));
         right_node -> parent = root;
 
@@ -224,7 +225,6 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         if (original_child_ptr != NULL) {
             memcpy(left_node -> children, original_child_ptr, (num_key_left + 1) * sizeof(struct tree_node));
         }
-        fprintf(stderr, "original kv pointer: %p\n", original_kv_ptr);
         memcpy(left_node -> pairs, original_kv_ptr, num_key_left * sizeof(struct kv_pair));
         left_node -> parent = root;
 
