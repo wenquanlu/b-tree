b6fd7c4af05a8d46b574e54aeed1133e276192a2
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 14:45:20 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -69,6 +69,8 @@ void close_store(void * helper) {
     fprintf(stderr, "the final root: %d\n", root -> pairs -> key);
     fprintf(stderr, "then left %d\n", root -> children -> pairs ->key);
     fprintf(stderr, "the right %d\n", (root -> children + 1) -> pairs -> key);
+    fprintf(stderr, "the left left %d\n", root -> children -> children -> pairs -> key);
+    fprintf(stderr, "the left right %d\n", (root -> children -> children + 1) -> pairs -> key);
     post_order_clean(root);
     free(helper);
     return;
