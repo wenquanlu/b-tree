31db30af15f83a64fad0f8bfd960e40131308375
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 14:14:58 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -203,6 +203,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         fprintf(stderr, "num key right: %d\n", num_key_right);
         fprintf(stderr, "original kv ptr: %p\n", original_kv_ptr);
         fprintf(stderr, "mi: %d\n", midindex);
+        fprintf(stderr, "original num keys: %d\n", original_num_keys);
         memcpy(right_node -> pairs, original_kv_ptr + midindex + 1, (num_key_right) * sizeof(struct kv_pair));
         right_node -> parent = parent;
 
