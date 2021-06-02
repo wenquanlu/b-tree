83d9aa138d2552fd2682796bdaa80146b4db29c8
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-02 14:10:10 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -159,6 +159,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
         int midindex_key = (root -> pairs)[midindex].key;
         struct tree_node * original_child_ptr = root -> children;
         struct kv_pair * original_kv_ptr = root -> pairs;
+        fprintf(stderr, "prey to god: %d\n", root -> pairs -> key);
         int original_num_keys = root -> num_keys;
         struct tree_node * parent = root -> parent;
         int counter = 0;
