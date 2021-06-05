4cebdb6d0a2acf4e8700b82023db2508ff6a19e5
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-05 13:59:17 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -616,6 +616,7 @@ void merge_from_right(struct tree_node * left_node, struct tree_node * right_nod
         }
     }
     /////////
+    fprintf(stderr, "leftn: %p\n", left_node);
     parent -> children = malloc(((parent -> num_keys) * sizeof(struct tree_node)));
     parent -> pairs = malloc(((parent -> num_keys) - 1) * sizeof(struct kv_pair));
     memcpy(parent -> pairs, original_parent_keys, inter_key_idx * sizeof(struct kv_pair));
@@ -634,6 +635,7 @@ void merge_from_right(struct tree_node * left_node, struct tree_node * right_nod
     for (int i = 0; i <= parent -> num_keys; i++) {
         struct tree_node * child = parent -> children + i;
         if (child -> children != NULL) {
+            fprintf(stderr, "chilchil: %p\n", child);
             for (int j = 0; j <= child -> num_keys; j++) {
                 (child -> children + j) -> parent = child;
             }
