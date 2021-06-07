37c9ae8da6db25b1f682e32fadf01d2b096f8366
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-07 22:05:13 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -88,7 +88,7 @@ int btree_insert(uint32_t key, void * plaintext, size_t count, uint32_t encrypti
     pthread_mutex_t * muteces = (pthread_mutex_t *) (info + 3);
     sem_t * w_sem = (sem_t *) (muteces + 1);
     x++;
-    if (x == 29961) {
+    if (x == 29963) {
         fprintf(stderr, "%d\n", *reading);
         fprintf(stderr, "29963!\n");
     }
