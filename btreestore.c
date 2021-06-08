a3deb58d81ab56571419dc11959003d3e8521b41
welu3614 <welu3614@uni.sydney.edu.au>
2021-06-08 18:41:52 +1000

    changed

--- btreestore.c
+++ btreestore.c
@@ -1095,7 +1095,7 @@ int pre_order(struct tree_node * root, int count, struct node ** ls) {
     }
     int root_num_keys = root -> num_keys;
     count ++; //?
-    if (count > 6500) {
+    if (count > 6900) {
         fprintf(stderr, "count: %d\n", count);
     }
     *ls = realloc(*ls, (count) * sizeof(struct node));
